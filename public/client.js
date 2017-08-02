ab8f7185be8d237d2bdc4246a5e89e030dd7f4b0
Luke Schlangen <lukeschlangen@gmail.com>
2017-08-02 11:32:41 -0500

    Adding new goose

--- public/client.js
+++ public/client.js
@@ -15,15 +15,42 @@ var geese = [
 
 $(document).ready(function(){
     console.log('jquery is loaded');
-    for (var i = 0; i < geese.length; i++) {
-        var goose = geese[i];
+    drawGeeseTable();
+
+    // Add new goose on button click
+    $('#newGooseButton').on('click', function(){
+        console.log('new goose button was clicked');
+        var newGoose = {}; // creates empty newGoose object
+
+        // Adding properties to the new goose
+        newGoose.name = $('#newGooseName').val();
+        newGoose.age = parseInt($('#newGooseAge').val());
+        newGoose.type = $('#newGooseType').val();
+
+        console.log('newGoose:', newGoose);
+
+        // Adding new goose to goose array
+        geese.push(newGoose);
         
-        $('#geesenTableBody').prepend(
-            '<tr>' +
-                '<td>' + goose.name + '</td>' +
-                '<td>' + goose.age + '</td>' +
-                '<td>' + goose.type + '</td>' +
-            '</tr>'
-        );
-    }
+        // redrawing the table with the new goose
+        drawGeeseTable();
+    });
+
+    function drawGeeseTable() {
+        // adding geese to DOM
+        $('#geesenTableBody').empty();
+        for (var i = 0; i < geese.length; i++) {
+            var goose = geese[i];
+            
+            $('#geesenTableBody').prepend(
+                '<tr>' +
+                    '<td>' + goose.name + '</td>' +
+                    '<td>' + goose.age + '</td>' +
+                    '<td>' + goose.type + '</td>' +
+                '</tr>'
+            );
+        }
+    } 
+    
 });
+
